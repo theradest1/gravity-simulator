225d82e6f356cadb6c289b3a7c85ab377210f63c
TheRad1 <18326@verona.k12.wi.us>
2023-07-18 15:18:10 -0500

    small changes

--- gravity.py
+++ gravity.py
@@ -376,3 +376,167 @@ while True:
 		frameCount = 0
 
 		pygame.display.flip() #update display
+
+"""solar system
+[
+	{
+		"name": "Sun",
+		"mass": 1.9885e30,
+		"radius": 6.96e8,
+		"initialXPos": 0,
+		"initialYPos": 0,
+		"initialXVel": 0,
+		"initialYVel": 0,
+		"color": [255, 150, 0],
+		"relativeTo": "None",
+		"light": 5
+	},
+	{
+		"name": "Mercury",
+		"mass": 0.33e24,
+		"radius": 2439500,
+		"initialXPos": 0,
+		"initialYPos": 57900e6,
+		"initialXVel": 47400,
+		"initialYVel": 0,
+		"color": [26, 26, 26],
+		"relativeTo": "Sun",
+		"light": 0
+	},
+	{
+		"name": "Venus",
+		"mass": 4.87e24,
+		"radius": 6052000,
+		"initialXPos": 0,
+		"initialYPos": 108200e6,
+		"initialXVel": 35000,
+		"initialYVel": 0,
+		"color": [230, 230, 230],
+		"relativeTo": "Sun",
+		"light": 0
+	},
+	{
+		"name": "Earth",
+		"mass": 5.97e24,
+		"radius": 6380000,
+		"initialXPos": 0,
+		"initialYPos": 1.496e11,
+		"initialXVel": 2.98e4,
+		"initialYVel": 0,
+		"color": [47, 106, 105],
+		"relativeTo": "Sun",
+		"light": 0
+	},
+	{
+		"name": "Moon",
+		"mass": 0.073e24,
+		"radius": 1737500,
+		"initialXPos": 0,
+		"initialYPos": 384e6,
+		"initialXVel": 1000,
+		"initialYVel": 0,
+		"color": [150, 150, 150],
+		"relativeTo": "Earth",
+		"light": 0
+	},
+	{
+		"name": "Mars",
+		"mass": 0.642e24,
+		"radius": 3396000,
+		"initialXPos": 0,
+		"initialYPos": 228.0e9,
+		"initialXVel": 24.1e3,
+		"initialYVel": 0,
+		"color": [153, 61, 0],
+		"relativeTo": "Sun",
+		"light": 0
+	},
+	{
+		"name": "Jupiter",
+		"mass": 1898e24,
+		"radius": 71492000,
+		"initialXPos": 0,
+		"initialYPos": 778.5e9,
+		"initialXVel": 13.1e3,
+		"initialYVel": 0,
+		"color": [176, 127, 53],
+		"relativeTo": "Sun",
+		"light": 0
+	},
+	{
+		"name": "Saturn",
+		"mass": 568e24,
+		"radius": 60268000,
+		"initialXPos": 0,
+		"initialYPos": 1432.0e9,
+		"initialXVel": 9.7e3,
+		"initialYVel": 0,
+		"color": [176, 143, 54],
+		"relativeTo": "Sun",
+		"light": 0
+	},
+	{
+		"name": "Uranus",
+		"mass": 86.8e24,
+		"radius": 25559000,
+		"initialXPos": 0,
+		"initialYPos": 2867.0e9,
+		"initialXVel": 6.8e3,
+		"initialYVel": 0,
+		"color": [85, 128, 170],
+		"relativeTo": "Sun",
+		"light": 0
+	},
+	{
+		"name": "Neptune",
+		"mass": 102e24,
+		"radius": 24764000,
+		"initialXPos": 0,
+		"initialYPos": 4515.0e9,
+		"initialXVel": 5.4e3,
+		"initialYVel": 0,
+		"color": [54, 104, 150],
+		"relativeTo": "Sun",
+		"light": 0
+	}
+]"""
+
+""" dobble:
+[
+	{
+		"name": "Sun-1",
+		"mass": 1.9885e30,
+		"radius": 6.96e8,
+		"initialXPos": 0,
+		"initialYPos": 0,
+		"initialXVel": -2e4,
+		"initialYVel": 0,
+		"color": [255, 150, 0],
+		"relativeTo": "None",
+		"light": 5
+	},
+	{
+		"name": "Sun-2",
+		"mass": 1.9885e30,
+		"radius": 6.96e8,
+		"initialXPos": 0,
+		"initialYPos": 1.496e11,
+		"initialXVel": 2e4,
+		"initialYVel": 0,
+		"color": [255, 150, 0],
+		"relativeTo": "None",
+		"light": 5
+	},
+	{
+		"name": "Planet",
+		"mass": 5.97e24,
+		"radius": 6380000,
+		"initialXPos": 0,
+		"initialYPos": 3e11,
+		"initialXVel": -3e4,
+		"initialYVel": 0,
+		"color": [47, 106, 105],
+		"relativeTo": "None",
+		"light": 0
+	}
+]"""
